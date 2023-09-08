a111f0a7fa6e38e21d6785e77a41562119249d97
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-09-08 11:12:06 -0500

    commit

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,10 +1,28 @@
 #include "binary_trees.h"
 
 /**
+ * heapify_up - Moves a newly inserted node up the heap if necessary
+ * @node: The node to move up
+ * Return: A pointer to the node after the heapify operation
+ */
+heap_t *heapify_up(heap_t *node)
+{
+	while (node->parent && node->n > node->parent->n)
+	{
+		int temp = node->n;
+		node->n = node->parent->n;
+		node->parent->n = temp;
+
+		node = node->parent;
+	}
+
+	return (node);
+}
+
+/**
  * heap_insert - Inserts a value into a Max Binary Heap
  * @root: A double pointer to the root node of the Heap
  * @value: The value to store in the node to be inserted
- *
  * Return: A pointer to the inserted node, or NULL on failure
  */
 heap_t *heap_insert(heap_t **root, int value)
@@ -21,57 +39,24 @@ heap_t *heap_insert(heap_t **root, int value)
 		return (new_node);
 	}
 
-	parent = find_insert_parent(*root);
-	if (!parent)
+	parent = *root;
+	while (parent->left && parent->right)
 	{
-		free(new_node);
-		return (NULL);
+		if (value <= parent->n)
+			parent = parent->left;
+		else
+			parent = parent->right;
 	}
 
 	if (!parent->left)
+	{
 		parent->left = new_node;
+	}
 	else
+	{
 		parent->right = new_node;
+	}
 
 	new_node->parent = parent;
 	return (heapify_up(new_node));
-}
-
-/**
- * find_insert_parent - Finds the parent node for inserting a new node
- * @root: A pointer to the root node of the Heap
- *
- * Return: A pointer to the parent node
- */
-heap_t *find_insert_parent(heap_t *root)
-{
-	while (root->left && root->right)
-	{
-		if (root->left->n > root->right->n)
-			root = root->left;
-		else
-			root = root->right;
-	}
-	return (root);
-}
-
-/**
- * heapify_up - Moves a newly inserted node up the heap if necessary
- * @node: The node to move up
- *
- * Return: A pointer to the node after the heapify operation
- */
-heap_t *heapify_up(heap_t *node)
-{
-	int temp;
-
-	while (node->parent && node->n > node->parent->n)
-	{
-		temp = node->n;
-		node->n = node->parent->n;
-		node->parent->n = temp;
-		node = node->parent;
-	}
-
-	return (node);
 }
