186dc111aa579208ce3c36a4777817b2138c4779
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-09-26 18:59:38 -0500

    commit

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -2,56 +2,57 @@
 
 import sys
 
-""" Module for printing metrics for a given metric. """
 
 def print_metrics(file_size, status_codes):
     """
-    print_metrics: Print the calculated metrics.
+    Print the calculated metrics.
 
     Args:
-        file_size: size of file.
-        status_codes: dictionary of status codes.
+        file_size (list): List of file sizes.
+        status_codes (dict): Dictionary of status codes and their counts.
     """
     total_size = sum(file_size)
     print(f"Total file size: {total_size}")
-    
+
     sorted_status_codes = sorted(status_codes.keys())
     for code in sorted_status_codes:
         count = status_codes[code]
         print(f"{code}: {count}")
 
+
 def main():
     """
-    main: Main function that calculates metrics.
+    The main function that processes input lines and calculates metrics.
     """
     line_count = 0
     file_size = []
     status_codes = {}
-    
+
     try:
         for line in sys.stdin:
             line = line.strip()
             parts = line.split()
-            
+
             if len(parts) != 10:
                 continue
-            
+
             ip, _, _, date, _, _, request, status, size, *_ = parts
-            
+
             if request != '"GET' or not status.isdigit():
                 continue
-            
+
             status = int(status)
             file_size.append(int(size))
             status_codes[status] = status_codes.get(status, 0) + 1
-            
+
             line_count += 1
-            
+
             if line_count % 10 == 0:
                 print_metrics(file_size, status_codes)
-    
+
     except KeyboardInterrupt:
         pass
+
 
 if __name__ == "__main__":
     main()
