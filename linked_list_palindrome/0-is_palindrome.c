cb6b53c69fde3067363028bda6659a768958ed88
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-09-26 17:04:41 -0500

    commit

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Definition for singly-linked list */
-typedef struct listint_s {
-    int n;
-    struct listint_s *next;
-} listint_t;
+
 
 int is_palindrome(listint_t **head)
 {
-    /* Check for empty list or list with only one element (considered a palindrome) */
+
     if (*head == NULL || (*head)->next == NULL) {
         return 1;
     }
@@ -20,20 +16,17 @@ int is_palindrome(listint_t **head)
     listint_t *mid = NULL;
     int is_palindrome = 1;
 
-    /* Use Floyd's Tortoise and Hare algorithm to find the middle of the list */
     while (fast_ptr != NULL && fast_ptr->next != NULL) {
         fast_ptr = fast_ptr->next->next;
         prev_slow_ptr = slow_ptr;
         slow_ptr = slow_ptr->next;
     }
 
-    /* If the list has an odd number of nodes, skip the middle node */
     if (fast_ptr != NULL) {
         mid = slow_ptr;
         slow_ptr = slow_ptr->next;
     }
 
-    /* Reverse the second half of the list */
     listint_t *second_half = NULL;
     listint_t *current = slow_ptr;
     listint_t *next = NULL;
@@ -45,7 +38,6 @@ int is_palindrome(listint_t **head)
         current = next;
     }
 
-    /* Compare the first half and the reversed second half */
     listint_t *first_half = *head;
     while (second_half != NULL) {
         if (first_half->n != second_half->n) {
@@ -56,7 +48,6 @@ int is_palindrome(listint_t **head)
         second_half = second_half->next;
     }
 
-    /* Restore the original list by reversing the reversed second half again */
     current = slow_ptr;
     next = NULL;
     while (current != NULL) {
@@ -66,7 +57,6 @@ int is_palindrome(listint_t **head)
         current = next;
     }
 
-    /* Reconnect the first half with the middle node (if exists) and the second half */
     if (mid != NULL) {
         prev_slow_ptr->next = mid;
         mid->next = second_half;
